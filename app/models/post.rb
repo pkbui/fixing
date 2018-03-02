3a71798524836f80a188a49c9bc06d97303837f9
Dat Tu <hoangtu_1232001@yahoo.com>
2018-03-02 03:18:26 +0000

    add feature

---
+++ app/models/post.rb
@@ -0,0 +1,11 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :user, presence: true
+  
+  def to_s
+      title
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :posts
+  root to: 'visitors#index'
+  devise_for :users
+  resources :users
+end
